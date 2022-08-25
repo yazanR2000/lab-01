65c67978693da7804916108ee80191ff5e54d1a6
yazanR2000 <yazanradaideh123@gmail.com>
2022-08-25 17:40:25 +0300

    this is the updated commit

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main(){
+  Map yazanInfo = {
+    "first_name" : "yazan",
+    "last_name" : "radaideh",
+    "age" : 22,
+    "education info" : {
+      "name" : "yarmouk university",
+      "faculty" : "11",
+      "year" : 2022
+    },
+  };
+}
